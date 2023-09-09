433e98a0ad24110c50602ed5c13c9bb824fee31b
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2023-09-10 01:17:21 +0300

    contact page

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,6 +1,30 @@
-import React from "react"
+import React, { useRef } from "react"
 import "./contact.css"
+import emailjs from "@emailjs/browser"
+
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        "service_4of3ezl",
+        "template_czboyab",
+        form.current,
+        "NNTr85XMcI-rzRlQ6"
+      )
+      .then(
+        (result) => {
+          alert("Thank you for contacting me")
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+
   return (
     <div className="contact">
       <div className="contactleft">
@@ -9,17 +33,31 @@ const Contact = () => {
       </div>
       <div className="cright">
         <div className="form">
-          <form className="mform">
-            <input className="cinput" type="text" placeholder="Name" />
-            <input className="cinput" type="email" placeholder="Email" id="" />
-            <textarea
+          <form className="mform" ref={form} onSubmit={sendEmail}>
+            <input
               className="cinput"
-              name=""
+              type="text"
+              placeholder="Name"
+              name="from_name"
+            />
+            <input
+              className="cinput"
+              type="email"
+              placeholder="Email"
+              id=""
+              name="user_email"
+            />
+            <textarea
+              className="ctextarea"
+              name="message"
               placeholder="Message"
               id=""
               cols="30"
               rows="2"
             ></textarea>{" "}
+            <div className="submit">
+              <input className="csubmit" type="submit" value="Send" />
+            </div>
           </form>
         </div>
       </div>
